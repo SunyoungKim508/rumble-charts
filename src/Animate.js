c8318fcd60aa2e659d4692bd8a063ba62c4461e7
SunyoungKim508 <sunyoungkim508@gmail.com>
2018-05-23 08:54:39 -0700

    Update to react 16.3.2

--- src/Animate.js
+++ src/Animate.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import _ from 'lodash';
-import {timer} from 'd3-timer';
-import {interpolate as d3Interpolate} from 'd3-interpolate';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import _ from 'lodash'
+import { timer } from 'd3-timer'
+import { interpolate as d3Interpolate } from 'd3-interpolate'
 
-import eases from './helpers/eases';
-import proxyChildren from './helpers/proxyChildren';
-import normalizeSeries from './helpers/normalizeSeries';
+import eases from './helpers/eases'
+import proxyChildren from './helpers/proxyChildren'
+import normalizeSeries from './helpers/normalizeSeries'
 
 /**
  * Animates (actually interpolates) your `series` data. Very useful when you want to have a simple transitions
@@ -17,78 +17,80 @@ import normalizeSeries from './helpers/normalizeSeries';
  * @example ../docs/examples/Animate.md
  */
 export default class Animate extends Component {
-
     constructor(props) {
-        super(props);
+        super(props)
 
-        this.state = _.assign({}, this.props, normalizeSeries(this.props));
+        this.state = _.assign({}, this.props, normalizeSeries(this.props))
     }
 
     // lifecycle
 
-    componentWillReceiveProps(nextProps) {
-
+    componentDidUpdate(prevProps, prevState) {
         const interpolate = d3Interpolate(
-            _.pick(this.state, this.props.interpolateProps),
+            _.pick(prevState, prevProps.interpolateProps),
             _.pick(
-                _.assign({}, nextProps, normalizeSeries(nextProps)),
-                this.props.interpolateProps
+                _.assign({}, this.props, normalizeSeries(this.props)),
+                prevProps.interpolateProps
             )
-        );
+        )
 
-        const {duration, onStart, onEnd, logFPS} = this.props;
-        let {ease} = this.props;
-        ease = _.isString(ease) ?
-            eases[ease] :
-            (_.isFunction(ease) ? ease : eases['linear']);
+        const { duration, onStart, onEnd, logFPS } = prevProps
+        let { ease } = prevProps
+        ease = _.isString(ease)
+            ? eases[ease]
+            : _.isFunction(ease) ? ease : eases['linear']
 
-        let i = 0;
-        this._timer && this._timer.stop();
-        onStart && onStart();
+        let i = 0
+        this._timer && this._timer.stop()
+        onStart && onStart()
         const _timer = timer(p => {
-            i++;
+            i++
             if (p >= duration) {
-                p = duration;
-                this.setState(interpolate(ease(p / duration)));
+                p = duration
+                this.setState(interpolate(ease(p / duration)))
 
-                onEnd && onEnd();
+                onEnd && onEnd()
                 if (logFPS) {
-                    console.warn(i * (1000 / duration) + 'fps; ' + i + ' frames in ' + duration + 'ms');
+                    console.warn(
+                        i * (1000 / duration) +
+                            'fps; ' +
+                            i +
+                            ' frames in ' +
+                            duration +
+                            'ms'
+                    )
                 }
-                _timer.stop();
+                _timer.stop()
             } else {
-                this.setState(interpolate(ease(p / duration)));
+                this.setState(interpolate(ease(p / duration)))
             }
-        });
-        this._timer = _timer;
+        })
+        this._timer = _timer
     }
 
     componentWillUnmount() {
-        this._timer && this._timer.stop();
+        this._timer && this._timer.stop()
     }
 
     // render
 
     render() {
-        const {props, state} = this;
+        const { props, state } = this
 
-        return <g className={props.className}>
-            {proxyChildren(
-                props.children,
-                _.omitBy(state, _.isUndefined),
-                {
+        return (
+            <g className={props.className}>
+                {proxyChildren(props.children, _.omitBy(state, _.isUndefined), {
                     layerWidth: state.layerWidth,
                     layerHeight: state.layerHeight,
                     scaleX: props.scaleX,
-                    scaleY: props.scaleY
-                }
-            )}
-        </g>;
+                    scaleY: props.scaleY,
+                })}
+            </g>
+        )
     }
-
 }
 
-Animate.displayName = 'Animate';
+Animate.displayName = 'Animate'
 
 Animate.propTypes = {
     /**
@@ -102,25 +104,73 @@ Animate.propTypes = {
     ease: PropTypes.oneOfType([
         PropTypes.func,
         PropTypes.oneOf([
-            'linear', 'poly', 'quad', 'cubic', 'sin', 'exp',
-            'circle', 'bounce', 'elastic', 'back',
-            'linear-in', 'poly-in', 'quad-in', 'cubic-in', 'sin-in', 'exp-in',
-            'circle-in', 'bounce-in', 'elastic-in', 'back-in',
-            'linear-out', 'poly-out', 'quad-out', 'cubic-out', 'sin-out', 'exp-out',
-            'circle-out', 'bounce-out', 'elastic-out', 'back-out',
-            'linear-in-out', 'poly-in-out', 'quad-in-out', 'cubic-in-out', 'sin-in-out', 'exp-in-out',
-            'circle-in-out', 'bounce-in-out', 'elastic-in-out', 'back-in-out',
-            'linear-out-in', 'poly-out-in', 'quad-out-in', 'cubic-out-in', 'sin-out-in', 'exp-out-in',
-            'circle-out-in', 'bounce-out-in', 'elastic-out-in', 'back-out-in'
-        ])
+            'linear',
+            'poly',
+            'quad',
+            'cubic',
+            'sin',
+            'exp',
+            'circle',
+            'bounce',
+            'elastic',
+            'back',
+            'linear-in',
+            'poly-in',
+            'quad-in',
+            'cubic-in',
+            'sin-in',
+            'exp-in',
+            'circle-in',
+            'bounce-in',
+            'elastic-in',
+            'back-in',
+            'linear-out',
+            'poly-out',
+            'quad-out',
+            'cubic-out',
+            'sin-out',
+            'exp-out',
+            'circle-out',
+            'bounce-out',
+            'elastic-out',
+            'back-out',
+            'linear-in-out',
+            'poly-in-out',
+            'quad-in-out',
+            'cubic-in-out',
+            'sin-in-out',
+            'exp-in-out',
+            'circle-in-out',
+            'bounce-in-out',
+            'elastic-in-out',
+            'back-in-out',
+            'linear-out-in',
+            'poly-out-in',
+            'quad-out-in',
+            'cubic-out-in',
+            'sin-out-in',
+            'exp-out-in',
+            'circle-out-in',
+            'bounce-out-in',
+            'elastic-out-in',
+            'back-out-in',
+        ]),
     ]),
     onStart: PropTypes.func,
-    onEnd: PropTypes.func
-};
+    onEnd: PropTypes.func,
+}
 
 Animate.defaultProps = {
-    interpolateProps: ['series', 'maxX', 'maxY', 'minX', 'minY', 'layerWidth', 'layerHeight'],
+    interpolateProps: [
+        'series',
+        'maxX',
+        'maxY',
+        'minX',
+        'minY',
+        'layerWidth',
+        'layerHeight',
+    ],
     proxyProps: ['seriesNormalized'],
     duration: 500,
-    ease: 'linear'
-};
+    ease: 'linear',
+}
